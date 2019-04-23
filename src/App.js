2e3acfdf2dc9cd6e86ca579ff875618675719035
hbtpoprock <krisnu.sut@gmail.com>
2019-04-24 03:46:24 +0700

    err-handler added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { type } from 'os';
 
 // class App extends Component {
 //   render() {
@@ -26,94 +27,126 @@ import './App.css';
 // }
 
 class App extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          input: 0,
-          submit: ''
-      };
-  }
-  handleChange(e) {
-      this.setState({
-          input: e.target.value,
-          submit: ''
-      })
-  }
-  increment() {
-      this.setState({
-          input: parseInt(this.state.input) + 1,
-          submit: ''
-      })
-  }
-  decrement() {
-      this.setState({
-          input: parseInt(this.state.input) - 1,
-          submit: ''
-      })
-  }
-  reset() {
-      this.setState({
-          input: 0,
-          submit: ''
-      })
-  }
-  handleSubmit(e) {
-      this.setState({
-          submit: this.state.input
-      })
-      console.log('submit')
-      e.preventDefault()
-  }
-  render() {
-      return (
-          <div>
-              <GetInput
-                  input={this.state.input}
-                  handleChange={this.handleChange.bind(this)}
-                  handleSubmit={this.handleSubmit.bind(this)}
-                  increment={this.increment.bind(this)}
-                  decrement={this.decrement.bind(this)}
-                  reset={this.reset.bind(this)}
-              />
-              <RenderInput input={this.state.submit} />
-          </div>
-      );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: 0,
+            submit: ''
+        };
+    }
+    handleChange(e) {
+        if (e.target.value.match(/^$/)) {
+            this.setState({
+                input: '',
+                submit: ''
+            })
+        }
+        else if (e.target.value.match(/^-$/)) {
+            this.setState({
+                input: "-",
+                submit: ''
+            })
+        }
+        else if (e.target.value.match(/^-?\d+$/)) {
+            this.setState({
+                input: parseInt(e.target.value),
+                submit: ''
+            })
+        }
+    }
+    increment() {
+        if (this.state.input !== '') {
+            this.setState({
+                input: parseInt(this.state.input) + 1,
+                submit: ''
+            })
+        } else {
+            this.setState({
+                submit: "That's not a number"
+            })
+        }
+    }
+    decrement() {
+        if (this.state.input !== '') {
+            this.setState({
+                input: parseInt(this.state.input) - 1,
+                submit: ''
+            })
+        } else {
+            this.setState({
+                submit: "That's not a number"
+            })
+        }
+    }
+    reset() {
+        this.setState({
+            input: 0,
+            submit: ''
+        })
+    }
+    handleSubmit(e) {
+        if (this.state.input === '') {
+            this.setState({
+                submit: "That's not a number"
+            })
+        } else {
+            this.setState({
+                submit: this.state.input
+            })
+        }
+        console.log('submit')
+        e.preventDefault()
+    }
+    render() {
+        return (
+            <div>
+                <GetInput
+                    input={this.state.input}
+                    handleChange={this.handleChange.bind(this)}
+                    handleSubmit={this.handleSubmit.bind(this)}
+                    increment={this.increment.bind(this)}
+                    decrement={this.decrement.bind(this)}
+                    reset={this.reset.bind(this)}
+                />
+                <RenderInput input={this.state.submit} />
+            </div>
+        );
+    }
 }
 
 class GetInput extends React.Component {
-  constructor(props) {
-      super(props);
-  }
-  render() {
-      return (
-          <div>
-              <input value={this.props.input} onChange={this.props.handleChange} />
-              <br />
-              <br />
-              <button className='inc' onClick={this.props.increment}>Increment!</button>
-              <button className='dec' onClick={this.props.decrement}>Decrement!</button>
-              <button className='reset' onClick={this.props.reset}>Reset</button>
-              <br />
-              <br />
-              <button onClick={this.props.handleSubmit} >Submit!</button>
-          </div>
-      );
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <input value={this.props.input} onChange={this.props.handleChange} />
+                <br />
+                <br />
+                <button className='inc' onClick={this.props.increment}>Increment!</button>
+                <button className='dec' onClick={this.props.decrement}>Decrement!</button>
+                <button className='reset' onClick={this.props.reset}>Reset</button>
+                <br />
+                <br />
+                <button onClick={this.props.handleSubmit} >Submit!</button>
+            </div>
+        );
+    }
 }
 
 class RenderInput extends React.Component {
-  constructor(props) {
-      super(props);
-  }
-  render() {
-      return (
-          <div>
-              <h3>Input Render:</h3>
-              <p>{this.props.input}</p>
-          </div>
-      );
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3>Input Render:</h3>
+                <p>{this.props.input}</p>
+            </div>
+        );
+    }
 }
 
 // ReactDOM.render(<MyApp />, document.getElementById('root'))
